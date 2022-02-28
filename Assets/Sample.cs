ca0eb6c05144490154878e94682b73dd2978b1f8
XJINE <umeda@team-lab.com>
2022-02-28 17:21:17 +0900

    First commit.

---
+++ Assets/Sample.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    private string _expression = "";
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(0, 0, 200, 200));
+
+        GUILayout.Label("Type Expression & Return");
+
+        GUI.SetNextControlName("TextField");
+
+        _expression = GUILayout.TextField(_expression);
+
+        var focused = GUI.GetNameOfFocusedControl() == "TextField";
+
+        GUILayout.EndArea();
+
+        if ((Event.current.isKey && Event.current.keyCode == KeyCode.Return) || !focused)
+        {
+            var (success, value) = ExpressionParser.Parse(_expression.Trim());
+        
+            if (success)
+            {
+                _expression = value.ToString();
+            }
+        }
+    }
+}
